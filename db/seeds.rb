63e466ac07008bb9b36fc879e794744b0dc1354a
Elizabeth Catoe <eccatoe2517@gmail.com>
2022-02-28 17:04:56 -0500

    commit tree b759a819747982414a79ccefd38502cd548cfe23

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Show.create( name: "Hey Arnold" , network: "Nickelodeon" , day: "Sunday" , rating: 9  )
+Show.create( name: "The Office" , network: "TBS" , day: "Tuesday" , rating: 8  )
+Show.create( name: "RuPaul" , network: "Netflix" , day: "Wednesday" , rating: 7  )
+Show.create( name: "South Park" , network: "Comedy Central" , day: "Sunday" , rating: 6  )
